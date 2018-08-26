f49240df0f42c8b1d752ccc2e2565c2233730789
David Ma <davidma415@gmail.com>
2018-08-26 10:58:16 -0400

    Finished exercise

---
+++ calculator.rb
@@ -0,0 +1,17 @@
+def add(n1, n2)
+  result = n1 + n2
+  return result
+end
+
+def subtract(n1, n2)
+  result = n1 - n2
+  return result
+end
+
+def sum(arr)
+  counter = 0
+  arr.each do |num|
+    counter += num
+  end
+  return counter
+end
